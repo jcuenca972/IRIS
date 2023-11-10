4828e9a9495df84ade2d193bf022aa1c548ace0d
Reem920 <145032986+Reem920@users.noreply.github.com>
2023-11-10 23:06:03 +0100

    Update adjusted_iris_classification_app.py

--- adjusted_iris_classification_app.py
+++ adjusted_iris_classification_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from joblib import load
 import numpy as np
 import pandas as pd
+import sklearn
 
 species = ['setosa', 'versicolor', 'virginica']
 image = ['setosa.jpg', 'versicolor.jpg', 'virginica.jpg']
